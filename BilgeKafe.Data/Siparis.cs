78392c9a66f7cfd69824862c20d956c99bf3ca62
Ogzhnsfgl <o.sofuoglu@yahoo.com>
2021-08-27 17:44:00 +0300

    BilgeKafe.Data katmanı oluşturuldu.

---
+++ BilgeKafe.Data/Siparis.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgeKafe.Data
+{
+    public class Siparis
+    { 
+        public int MasaNo { get; set; }
+
+        public SiparisDurum Durum { get; set; }
+
+        public decimal OdenenTutar { get; set; }
+        //DateTime'ları nullable yaptık (?) ile.
+        //Bu sayede Datetime null alabilir.
+        //= ile default değeri dateTime.now verdik.
+        // 2. Yöntem constructor içinde ayarlamak.
+        public DateTime? AcilisZamani { get; set; } =DateTime.Now;
+        public DateTime? KapanisZamani { get; set; }
+        public List<SiparisDetay> SiparisDetaylar { get; set; }
+        public string ToplamTutarTL => $"{ToplamTutarTL}₺";
+
+        public decimal ToplamTutar()
+        {
+            return SiparisDetaylar.Sum(siparis => siparis.Tutar());
+
+
+            //decimal total = 0;
+            //foreach (SiparisDetay detay in SiparisDetaylar)
+            //{
+            //    total=detay.Tutar();
+            //}
+            //return total;
+        }
+    }
+}
---
+++ BilgeKafe.Data/SiparisDetay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgeKafe.Data
+{
+    public class SiparisDetay
+    {
+        public string UrunAd { get; set; }
+        public  decimal BirimFiyat { get; set; }
+        public  int Adet { get; set; }
+
+        //Get Tutar return edecek.
+        public string TutarTL => $"{Tutar()}₺";
+
+        public decimal Tutar()
+        {
+            return BirimFiyat * Adet;
+        }
+    }
+}
